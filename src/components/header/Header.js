7cdf4ec4818bcf596a28e7548995e4480e43560f
Zidan <midoz393@gmail.com>
2020-03-08 15:10:41 -0400

    added cart component, as well as cart redux state management

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebaseUtil';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/CartIcon';
+import Cart from '../cart-dropdown/CartDropdown';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -26,12 +28,15 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : <Cart />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -6,8 +6,8 @@ const Preview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title}</h1>
         <div className='preview'>
-            {items.filter((item, index) => index < 4).map(({id, ...otherProps}) => (
-                <CollectionItem key={id} {...otherProps} />
+            {items.filter((item, index) => index < 4).map((item) => (
+                <CollectionItem key={item.id} item={item} />
             ))}
         </div>
     </div>
