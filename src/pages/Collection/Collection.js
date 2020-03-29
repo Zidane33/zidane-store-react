4eade4625c1dd64a0ae6e064634f10b0b613ea82
Zidan <midoz393@gmail.com>
2020-03-29 15:32:31 -0400

    added url paramater shop pages

--- src/pages/Collection/Collection.js
+++ src/pages/Collection/Collection.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { selectCollection } from '../../redux/shop/shop-selector';
+import { getCollectionsPending, getCollectionsError, getCollections } from '../../redux/shop/shop-selector';
+import { fetchCollections } from '../../redux/shop/shop-reducer';
 import CollectionItem from '../../components/collection-item/CollectionItem';
 import './collection.styles.scss';
+import {renderComponent} from '../../redux/shop/shop-actions';
 
-const Collection = ({ collection: { title, items } }) => (
-    <div className='collection-page'>
-        <h2 className='title'>{title}</h2>
-        <div className='items'>
-            {
-                items.map(item => (
-                    <CollectionItem key={item.id} item={item} />
-                ))
-            }
-        </div>
-    </div>
-)
+class Collection  extends React.Component {
+
+    componentDidMount(){
+        this.props.dispatch(fetchCollections());
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(true){
+            this.props.dispatch(renderComponent());
+        }
+    }
+    
+    render(){
+        const { error, pending, collection, match: { params: { collectionId } } } = this.props;
+
+        if (pending | error){
+            return (<h1>Loading</h1>)
+        }
+
+        return (
+            <div className='collection-page'>
+                <h2 className='title'>{collectionId}</h2>
+                <div className='items'>
+                    {
+                        collection.filter(collection => collection.collection.route === collectionId).map(item => (
+                            <CollectionItem key={item.id} item={item} />
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
+    collection: getCollections(state),
+    pending: getCollectionsPending(state),
+    error: getCollectionsError(state)
 }); 
 
 export default connect(mapStateToProps)(Collection);
--- src/redux/shop/shop-selector.js
+++ src/redux/shop/shop-selector.js
@@ -4,9 +4,3 @@ const selectShop = state => state.shop;
 export const getCollections = createSelector([selectShop], shop => shop.collection);
 export const getCollectionsPending = createSelector([selectShop], shop => shop.pending);
 export const getCollectionsError = createSelector([selectShop], shop => shop.error);
-
-export const selectShopCollections = createSelector([selectShop], shop => shop.collections);
-
-export const selectShopCollectionsForPreview = createSelector([selectShopCollections], collections => Object.keys(collections).map(key => collections[key]));
-
-export const selectCollection = collectionUrlParam => createSelector([selectShopCollections], collections => collections[collectionUrlParam]);
