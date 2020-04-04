80cd2c8ce1213bd90fdfc334884e6b109d54f68f
Zidan <midoz393@gmail.com>
2020-04-04 15:01:39 -0400

    fix to connect stripe client request to server

--- src/components/stripe/StripeButton.js
+++ src/components/stripe/StripeButton.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const apiKey = 'pk_test_8tSD0goXvaKagsr3RPuO91zc00Ke7CLlKs';
+
+    const onToken = token => {
+        console.log(token);
+        axios({
+            method: 'POST',
+            url: 'http://localhost:8080/charge',
+            data: ({
+                stripeToken: token.id,
+                amount: priceForStripe
+            }),
+        }).then(res => console.log(res))
+            .catch(e => console.log(e))
+    }
 
     return (
         <StripeCheckout
@@ -15,6 +29,7 @@ const StripeCheckoutButton = ({ price }) => {
             amount={priceForStripe}
             panelLabel='Pay Now'
             stripeKey={apiKey}
+            token={onToken}
         />
     )
     
