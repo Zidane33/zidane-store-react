0351ae9e9db84148287215c7018a9602aab911ba
Zidan <midoz393@gmail.com>
2020-04-27 12:24:14 -0400

    Style changes to homepage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebaseUtil';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -13,14 +12,10 @@ import './header.styles.scss';
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
-            <Logo className='logo' />
         </Link>
         <div className='options'>
             <Link className='option' to="/shop">
             SHOP
-            </Link>
-            <Link className='option' to="/shop">
-            CONTACT
             </Link>
             {
                 currentUser ? <div className='option' onClick={() => auth.signOut()}>
