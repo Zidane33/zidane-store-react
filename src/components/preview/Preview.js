0abda01666ac223e429b6c2a196d1b45033bb723
Zidan <midoz393@gmail.com>
2020-04-28 11:16:50 -0400

    added links to preview component

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CollectionItem from '../collection-item/CollectionItem';
 import './collection-preview.styles.scss';
+import { Link } from 'react-router-dom';
 
 const Preview = (items) => (
     <div className='collection-preview'>
-        <h1 className='title'>{items.items[0].collection.title}</h1>
+        <Link to={`/shop/${items.items[0].collection.title.toLowerCase()}`}>
+            <h1 className='title'>{items.items[0].collection.title}</h1>
+        </Link>
         <div className='preview'>
             {items.items.filter((items, index) => index < 4).map((item) => (
                 <CollectionItem key={item.id} item={item} />
