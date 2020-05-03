9cb866c13dc8682d4856454ed506f9ebeb4425c4
Zidan <midoz393@gmail.com>
2020-05-03 12:40:51 -0400

    added unit test for user login action

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import fetch from 'jest-fetch-mock';
 import { fetchSections } from '../redux/directory/directory-reducer';
 import { fetchCollections } from '../redux/shop/shop-reducer';
+import setCurrentUser from '../redux/user/user-actions';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
@@ -58,6 +59,25 @@ describe('testing fetch calls to backend', () => {
         store.dispatch(fetchCollections()).then(() => {
             return expect(store.getActions()[0].type).toEqual('FETCH_COLLECTIONS_SUCCESS')
         }).catch(e => console.log(e));
+    })
+})
+
+describe('testing user login', () => {
+
+    it('testing setting current user redux action', () => {
+        const store = mockStore({})
+
+        fetch.mockResponse('/', {
+            status: 200
+        })
+
+        const expectedActions = [{
+            type: 'SET_CURRENT_USER',
+            payload: 'user'
+        }] 
+
+        store.dispatch(setCurrentUser('user'))
+        return expect(store.getActions()).toEqual(expectedActions);
     })
 })
 
